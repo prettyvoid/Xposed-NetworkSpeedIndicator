e7055311b64581d74159e0284bccd490ac9bb154
FatMinMin <chiehmin18@gmail.com>
2014-02-12 21:36:03 +0800

    commit

--- src/tw/fatminmin/xposed/networkspeedindicator/Module.java
+++ src/tw/fatminmin/xposed/networkspeedindicator/Module.java
@@ -17,88 +17,135 @@ import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class Module implements IXposedHookInitPackageResources {
+public class Module implements IXposedHookLoadPackage,
+        IXposedHookInitPackageResources {
+
+    
+    
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        if(!lpparam.packageName.equals(PKG_NAME_SYSTEM_UI)) {
+            return;
+        }
+        try {
+            Class<?> c = XposedHelpers.findClass("com.android.systemui.statusbar.phone.PhoneStatusBar", lpparam.classLoader);
+            XposedBridge.hookAllMethods(c, "refreshAllStatusBarIcons", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param)
+                        throws Throwable {
+                    
+                    if(clock != null && trafficView != null) {
+                        trafficView.setTextColor(clock.getCurrentTextColor());
+                    }
+                    XposedBridge.log("hahahah");
+                }
+            });
+        }
+        catch(Exception e){
+            
+        }
+    }
     
     TrafficView trafficView;
+    TextView clock;
+
+    public static final String PKG_NAME_SYSTEM_UI = "com.android.systemui";
+
+    private static Map<String, String> mLayouts;
+    static {
+        Map<String, String> tmpMap = new HashMap<String, String>();
+        tmpMap.put("tw_super_status_bar", "statusIcons");
+        tmpMap.put("status_bar", "statusIcons");
+
+        mLayouts = Collections.unmodifiableMap(tmpMap);
+    }
     
-	public static final String PKG_NAME_SYSTEM_UI = "com.android.systemui";
+    @Override
+    public void handleInitPackageResources(InitPackageResourcesParam resparam)
+            throws Throwable {
+        if (!resparam.packageName.equals(PKG_NAME_SYSTEM_UI)) {
+            return;
+        }
+        XResources res = resparam.res;
 
-	private static Map<String, String> mLayouts;
-	static {
-		Map<String, String> tmpMap = new HashMap<String, String>();
-		tmpMap.put("tw_super_status_bar", "statusIcons");
-		tmpMap.put("status_bar", "statusIcons");
+        final Entry<String, String> layoutInfo = findLayoutInfo(res);
+        if (layoutInfo == null)
+            return;
 
-		mLayouts = Collections.unmodifiableMap(tmpMap);
-	}
-	
-	@Override
-	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
-		if (!resparam.packageName.equals(PKG_NAME_SYSTEM_UI))
-			return;
-		XResources res = resparam.res;
+        res.hookLayout(PKG_NAME_SYSTEM_UI, "layout", layoutInfo.getKey(),
+                new XC_LayoutInflated() {
 
-		final Entry<String, String> layoutInfo = findLayoutInfo(res);
-		if (layoutInfo == null)
-			return;
+                    @Override
+                    public void handleLayoutInflated(LayoutInflatedParam liparam)
+                            throws Throwable {
+                        FrameLayout root = (FrameLayout) liparam.view;
 
-		res.hookLayout(PKG_NAME_SYSTEM_UI, "layout", layoutInfo.getKey(), new XC_LayoutInflated() {
+                        clock = (TextView) root
+                                .findViewById(liparam.res.getIdentifier(
+                                        "clock", "id", PKG_NAME_SYSTEM_UI));
+                        if (trafficView == null) {
+                            trafficView = new TrafficView(root.getContext());
+                        }
+                        if (clock != null) {
+                            trafficView.setLayoutParams(clock.getLayoutParams());
+                            trafficView.setTextColor(clock
+                                    .getCurrentTextColor());
+                        } else {
+                            // gingerbread
+                            trafficView.setLayoutParams(new LayoutParams(
+                                    LayoutParams.WRAP_CONTENT,
+                                    LayoutParams.MATCH_PARENT));
+                            trafficView.setTextColor(Color
+                                    .parseColor("#33b5e5"));
+                        }
+                        trafficView.setGravity(Gravity.RIGHT
+                                | Gravity.CENTER_VERTICAL);
 
-			@Override
-			public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-				FrameLayout root = (FrameLayout) liparam.view;
-				
-				TextView clock = (TextView) root.findViewById(liparam.res.getIdentifier("clock", "id",
-						PKG_NAME_SYSTEM_UI));
-				if(trafficView == null) {
-				    trafficView = new TrafficView(root.getContext());
-				}
-				if(clock != null) {
-				    trafficView.setLayoutParams(clock.getLayoutParams());
-				    trafficView.setTextColor(clock.getCurrentTextColor());
-				}
-				else {
-				    // gingerbread
-				    trafficView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
-				    trafficView.setTextColor(Color.parseColor("#33b5e5"));
-				}
-				trafficView.setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
-				
-				
-                if(liparam.res.getIdentifier("status_bar_contents", "id",PKG_NAME_SYSTEM_UI) != 0) { 
-                    // kitkat
-                    trafficView.mPositionCallback = new PositionCallbackImpl();
-                }
-                else if(liparam.res.getIdentifier("notification_icon_area", "id",PKG_NAME_SYSTEM_UI) != 0) { 
-                    // jellybean
-                    trafficView.mPositionCallback = new JellyBeanPositionCallbackImpl();
-                }
-                else if(liparam.res.getIdentifier("notificationIcons", "id",PKG_NAME_SYSTEM_UI) != 0) {
-                    // gingerbread
-                    trafficView.mPositionCallback = new GingerBreadPositionCallbackImpl();
-                }
-                
-                
-                trafficView.mPositionCallback.setup(liparam, trafficView);
-                trafficView.refreshPosition();
-				
-			}
-		});
-	}
+                        if (liparam.res.getIdentifier("status_bar_contents",
+                                "id", PKG_NAME_SYSTEM_UI) != 0) {
+                            // kitkat
+                            trafficView.mPositionCallback = new PositionCallbackImpl();
+                        } else if (liparam.res.getIdentifier(
+                                "notification_icon_area", "id",
+                                PKG_NAME_SYSTEM_UI) != 0) {
+                            // jellybean
+                            trafficView.mPositionCallback = new JellyBeanPositionCallbackImpl();
+                        } else if (liparam.res.getIdentifier(
+                                "notificationIcons", "id", PKG_NAME_SYSTEM_UI) != 0) {
+                            // gingerbread
+                            trafficView.mPositionCallback = new GingerBreadPositionCallbackImpl();
+                        }
 
-	public static Entry<String, String> findLayoutInfo(XResources res) {
-		Iterator<Entry<String, String>> iterator = mLayouts.entrySet().iterator();
+                        trafficView.mPositionCallback.setup(liparam,
+                                trafficView);
+                        trafficView.refreshPosition();
 
-		while (iterator.hasNext()) {
-			Entry<String, String> entry = iterator.next();
-			if (res.getIdentifier(entry.getKey(), "layout", PKG_NAME_SYSTEM_UI) != 0
-					&& res.getIdentifier(entry.getValue(), "id", PKG_NAME_SYSTEM_UI) != 0)
-				return entry;
-		}
+                    }
+                });
+    }
 
-		return null;
-	}
+    public static Entry<String, String> findLayoutInfo(XResources res) {
+        Iterator<Entry<String, String>> iterator = mLayouts.entrySet()
+                .iterator();
+
+        while (iterator.hasNext()) {
+            Entry<String, String> entry = iterator.next();
+            if (res.getIdentifier(entry.getKey(), "layout", PKG_NAME_SYSTEM_UI) != 0
+                    && res.getIdentifier(entry.getValue(), "id",
+                            PKG_NAME_SYSTEM_UI) != 0)
+                return entry;
+        }
+
+        return null;
+    }
+
+    
 }
